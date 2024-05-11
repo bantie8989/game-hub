e40bcb216a548b2dba5a001729d6a3cdb6aa3d46
bantie8989 <146993130+bantie8989@users.noreply.github.com>
2024-05-11 16:34:11 +0300

    build color mode swich

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
 import {
-  HStack
+  HStack,Image
 } from "@chakra-ui/react";
 import ColorMode from "./ColorMode";
+import logo from "../assets/jungle.jpg"
 
 function NavBar() {
   return (
     <div>
-      <HStack boxSize="60px">
-        <img src="src\assets\jungle.jpg" alt="" />
+      <HStack  justifyContent="space-between" pl={2} pr={2} >
+        <Image  src={logo} boxSize="60px" />
         <ColorMode/>
       </HStack>
     </div>
