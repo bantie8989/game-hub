459e81edc7023267c9958c585fef05577127db12
bantie8989 <146993130+bantie8989@users.noreply.github.com>
2024-05-11 14:41:12 +0300

    build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,29 @@
-import "./App.css";
-import {
-  Box,
-  Step,
-  StepDescription,
-  StepIcon,
-  StepIndicator,
-  StepNumber,
-  StepSeparator,
-  StepStatus,
-  StepTitle,
-  Stepper,
-  useSteps,
-} from '@chakra-ui/react';
-
-
-
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
-  const steps = [
-    { title: 'First', description: 'Contact Info' },
-    { title: 'Second', description: 'Date & Time' },
-    { title: 'Third', description: 'Select Rooms' },
-  ]
-  
-  function Example() {
-    const { activeStep } = useSteps({
-      index: 1,
-      count: steps.length,
-    })
-  
-    return (
-      <Stepper index={activeStep}>
-        {steps.map((step, index) => (
-          <Step key={index}>
-            <StepIndicator>
-              <StepStatus
-                complete={<StepIcon />}
-                incomplete={<StepNumber />}
-                active={<StepNumber />}
-              />
-            </StepIndicator>
-  
-            <Box flexShrink='0'>
-              <StepTitle>{step.title}</StepTitle>
-              <StepDescription>{step.description}</StepDescription>
-            </Box>
-  
-            <StepSeparator />
-          </Step>
-        ))}
-      </Stepper>
-    )
-  }
-  
- 
-  return <div className="app"> <Example/>
-  </div>;
+  return (
+    <div>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav " "aside main"`,
+        }}
+      >
+        <GridItem bg="blue.300" area={"nav "}>
+          nav
+        </GridItem>
+        <Show above="lg">
+        
+          <GridItem bg="red.300" area={"aside "}>
+            aside
+          </GridItem>
+        </Show>
+
+        <GridItem bg="yellow.300" area={"main "}>
+          main
+        </GridItem>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
