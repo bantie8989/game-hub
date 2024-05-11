9f42c673a416f28b56ce2c9dd548dc0de7ba35c6
bantie8989 <146993130+bantie8989@users.noreply.github.com>
2024-05-11 16:18:44 +0300

    toggle color mode using switch

---
+++ src/components/ColorMode.tsx
@@ -0,0 +1,16 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+function ColorMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <div>
+      <HStack>
+        <Switch colorScheme="teal" size="lg" onChange={toggleColorMode} />
+        <Text> {colorMode === "light" ? "Dark" : "light"} Mode</Text>
+      </HStack>
+    </div>
+  );
+}
+
+export default ColorMode;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,14 @@
-import { Stack, HStack, VStack, Box } from "@chakra-ui/react";
+import {
+  HStack
+} from "@chakra-ui/react";
+import ColorMode from "./ColorMode";
 
 function NavBar() {
   return (
     <div>
-      <HStack boxSize="60px" >
-        <img src="src\assets\jungle.jpg"  alt="" />
-        <text>Navbar</text>
+      <HStack boxSize="60px">
+        <img src="src\assets\jungle.jpg" alt="" />
+        <ColorMode/>
       </HStack>
     </div>
   );
